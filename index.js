b6db469d1c8845d0c54d9c29835731a513d150d3
ngkao <ng.kao.ca@gmail.com>
2023-08-27 13:55:37 -0400

    add products and customers routes

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// DATA IMPORTS
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+
 // CONFIGURATIONS
 dotenv.config();
 const app = express();
@@ -36,5 +42,10 @@ mongoose
     })
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
+        // ONLY ADD DATA ONE TIME
+        // User.insertMany(dataUser)
+        // Product.insertMany(dataProduct);
+        // ProductStat.insertMany(dataProductStat);
     })
     .catch((error) => console.log(`${error} did not connect`))
